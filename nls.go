ca24b45cb176d1b8e316f092b6a79dcfe7460da6
David Golub <david.golub@mongodb.com>
2014-11-17 15:42:31 -0500

    Add resource update and NLS support.

---
+++ nls.go
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2014 MongoDB, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the license is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package gowin32
+
+import (
+	"github.com/winlabs/gowin32/wrappers"
+
+	"syscall"
+)
+
+type LocaleNameFlags uint32
+
+const (
+	LocaleNameAllowNeutralNames LocaleNameFlags = wrappers.LOCALE_ALLOW_NEUTRAL_NAMES
+)
+
+type Language uint16
+type Locale   uint32
+
+func (locale Locale) Language() Language {
+	return Language(wrappers.LangIDFromLCID(uint32(locale)))
+}
+
+func LocaleFromLocaleName(localeName string, flags LocaleNameFlags) (Locale, error) {
+	lcid, err := wrappers.LocaleNameToLCID(
+		syscall.StringToUTF16Ptr(localeName),
+		uint32(flags))
+	if err != nil {
+		return 0, err
+	}
+	return Locale(lcid), nil
+}
---
+++ wrappers/winnls.go
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2014 MongoDB, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the license is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package wrappers
+
+import (
+	"syscall"
+	"unsafe"
+)
+
+const (
+	LOCALE_NOUSEROVERRIDE        = 0x80000000
+	LOCALE_USE_CP_ACP            = 0x40000000
+	LOCALE_RETURN_NUMBER         = 0x20000000
+	LOCALE_RETURN_GENITIVE_NAMES = 0x10000000
+	LOCALE_ALLOW_NEUTRAL_NAMES   = 0x08000000
+)
+
+var (
+	procLocaleNameToLCID = modkernel32.NewProc("LocaleNameToLCID")
+)
+
+func LocaleNameToLCID(name *uint16, flags uint32) (uint32, error) {
+	r1, _, e1 := procLocaleNameToLCID.Call(
+		uintptr(unsafe.Pointer(name)),
+		uintptr(flags))
+	if r1 == 0 {
+		if e1.(syscall.Errno) != 0 {
+			return 0, e1
+		} else {
+			return 0, syscall.EINVAL
+		}
+	}
+	return uint32(r1), nil
+}
