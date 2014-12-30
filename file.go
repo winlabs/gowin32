49684c2f7b068ec49a93ae01a091c32d4c5783b4
David Golub <david.golub@mongodb.com>
2014-12-30 16:56:30 -0500

    Add TouchFile function.

---
+++ file.go
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2014 MongoDB, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the license is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package gowin32
+
+import (
+	"github.com/winlabs/gowin32/wrappers"
+
+	"os"
+	"syscall"
+)
+
+func TouchFile(f *os.File) error {
+	var now wrappers.FILETIME
+	wrappers.GetSystemTimeAsFileTime(&now)
+	if err := wrappers.SetFileTime(syscall.Handle(f.Fd()), nil, &now, &now); err != nil {
+		return NewWindowsError("SetFileTime", err)
+	}
+	return nil
+}
