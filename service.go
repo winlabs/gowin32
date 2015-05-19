c258ce1716fdfab97719186d25b4bde154276748
David Golub <david.golub@mongodb.com>
2015-05-19 14:36:46 -0400

    Add helper to check if a Windows service is running.

--- service.go
+++ service.go
@@ -497,3 +497,31 @@ func (self *SCManager) OpenService(serviceName string) (*Service, error) {
 	}
 	return &Service{handle: handle}, nil
 }
+
+func IsServiceRunning(serviceName string) (bool, error) {
+	// This function requires fewer access rights than using the above classes and can be used to check if a service
+	// is running without administrator access.
+	scmhandle, err := wrappers.OpenSCManager(
+		nil,
+		syscall.StringToUTF16Ptr(wrappers.SERVICES_ACTIVE_DATABASE),
+		wrappers.SC_MANAGER_CONNECT)
+	if err != nil {
+		return false, NewWindowsError("OpenSCManager", err)
+	}
+	defer wrappers.CloseServiceHandle(scmhandle)
+	handle, err := wrappers.OpenService(
+		scmhandle,
+		syscall.StringToUTF16Ptr(serviceName),
+		wrappers.SERVICE_QUERY_STATUS)
+	if err == wrappers.ERROR_SERVICE_DOES_NOT_EXIST {
+		return false, nil
+	} else if err != nil {
+		return false, NewWindowsError("OpenService", err)
+	}
+	defer wrappers.CloseServiceHandle(handle)
+	var status wrappers.SERVICE_STATUS
+	if err := wrappers.QueryServiceStatus(handle, &status); err != nil {
+		return false, NewWindowsError("QueryServiceStatus", err)
+	}
+	return status.CurrentState == wrappers.SERVICE_RUNNING, nil
+}
